8f361d2ef8a236077e2c7fc08c5688e61aa6e983
jaygeli <jaygeli@tencent.com>
2021-03-08 17:36:15 +0800

    添加verifyfirstlist条件

--- pactverify/utils.py
+++ pactverify/utils.py
@@ -3,7 +3,8 @@ import json
 from pactverify.core import Like, EachLike
 
 
-def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=False, separator='$', matchcol=[]):
+def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=False, separator='$', matchcol=[],
+                                   verifyfirstlist=True):
     """
     根据接口返回数据自动生成json格式断言数据
     :param target_data:  返回数据
@@ -11,9 +12,7 @@ def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=Fa
     :param is_list:
     :return:
     """
-
     base_types = (float, str, int, bool)
-
     like_key = separator + Like.__name__
     eachlike_key = separator + EachLike.__name__
 
@@ -40,6 +39,7 @@ def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=Fa
             pactverify_json = {like_key: copy.deepcopy(target_data)}
 
         for k, v in target_data.items():
+
             if (is_list):
                 target_data[k] = {}
             else:
@@ -61,12 +61,13 @@ def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=Fa
                     continue
                 if (is_list):
                     pactverify_json[k] = generate_pact_json_by_response(v, pactverify_json[k], False, separator,
-                                                                        matchcol)
+                                                                        matchcol, verifyfirstlist)
                 else:
                     pactverify_json[like_key][k] = generate_pact_json_by_response(v,
                                                                                   pactverify_json[like_key][
                                                                                       k],
-                                                                                  False, separator, matchcol)
+                                                                                  False, separator, matchcol,
+                                                                                  verifyfirstlist)
     elif type(target_data) == list:
         if len(target_data) == 0:
             pactverify_json = {
@@ -82,9 +83,28 @@ def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=Fa
             pactverify_json = {
                 eachlike_key: example_data
             }
-            pactverify_json[eachlike_key] = generate_pact_json_by_response(example_data,
-                                                                           pactverify_json[eachlike_key],
-                                                                           True, separator, matchcol)
+            if type(example_data) == dict:
+                value_type = {}
+            elif type(example_data) == list:
+                value_type = []
+            elif type(example_data) == str:
+                value_type = ""
+            elif type(example_data) == int:
+                value_type = 1
+            elif type(example_data) == bool:
+                value_type = True
+            if verifyfirstlist:
+                pactverify_json[eachlike_key] = {
+                    values_key: value_type,
+                    params_key: {
+                        "minimum": 1
+                    }
+                }
+            else:
+                pactverify_json[eachlike_key] = generate_pact_json_by_response(example_data,
+                                                                               pactverify_json[eachlike_key],
+                                                                               True, separator, matchcol,
+                                                                               verifyfirstlist)
 
 
     elif type(target_data) == type(None):
@@ -108,33 +128,23 @@ def generate_pact_json_by_response(target_data, pactverify_json=None, is_list=Fa
 
 
 if __name__ == '__main__':
-    response_json = {
-        "msg": "success",
-        "code": 0,
-        "data": [{
-            "type_id": 249,
-            "name": "王者荣耀",
-            "order_index": 1,
-            "status": 1,
-            "subtitle": " ",
-            "game_name": "王者荣耀"
-        }, {
-            "type_id": 250,
-            "name": "绝地求生",
-            "order_index": 2,
-            "status": 1,
-            "subtitle": " ",
-            "game_name": "绝地求生"
-        }, {
-            "type_id": 251,
-            "name": "刺激战场",
-            "order_index": 3,
-            "status": 1,
-            "subtitle": " ",
-            "game_name": "刺激战场"
-        }
-        ]
-    }
+    response_json = {"code": 0, "msg": "success", "ret": 0,
+                     "args": {"err": {"errorCode": -1, "errorMsg": "SUCCESS", "errorLevel": 0},
+                              "res": {"code": 0, "message": "", "items": [{"taskid": 1}], "firstInItems": [],
+                                      "timeItems": [{"taskId": 59}, {"taskId": 47, }]},
+                              "rsp": {"code": 0, "message": "", "items": [], "firstInItems": [], "timeItems": []}}}
     # 参数说明：响应json数据,契约关键字标识符(默认$)
-    pact_json = generate_pact_json_by_response(response_json, separator='$', matchcol=["code", "msg"])
-    print(pact_json)
+    from pactverify.matchers import PactJsonVerify
+    actual_data = copy.deepcopy(response_json)
+    expect_format = generate_pact_json_by_response(response_json, separator='$', matchcol=["code", "msg", 'errorCode'],
+                                                   verifyfirstlist=True)
+
+    mPactJsonVerify = PactJsonVerify(expect_format, hard_mode=False, separator='$')
+    # # 校验实际返回数据
+    mPactJsonVerify.verify(actual_data)
+    # 校验结果
+    verify_result = mPactJsonVerify.verify_result
+    if not verify_result:
+        raise Exception('字段不一致,错误信息:{0}'.format(mPactJsonVerify.verify_info))
+    else:
+        print(verify_result)
